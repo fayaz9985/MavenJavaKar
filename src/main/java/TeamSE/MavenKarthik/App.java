1bceee28d91746a012f793491313622cb758cd69
fayaz9985 <f6133942@gmail.com>
2025-01-01 17:13:22 +0530

    MavenJava Project

---
+++ src/main/java/TeamSE/MavenKarthik/App.java
@@ -0,0 +1,13 @@
+package TeamSE.MavenKarthik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! HELLO KARTHIK" );
+    }
+}
